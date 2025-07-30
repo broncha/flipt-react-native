51125478565d79826379303a3c65688d20ffce4f
Rajesh <broncha@rajesharma.com>
2025-07-30 17:29:57 +0545

    initial commit

---
+++ ios/Flipt.h
@@ -0,0 +1,5 @@
+#import <FliptSpec/FliptSpec.h>
+
+@interface Flipt : NSObject <NativeFliptSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import Flipt from './NativeFlipt';
+
+export function multiply(a: number, b: number): number {
+  return Flipt.multiply(a, b);
+}
